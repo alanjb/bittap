6a17bd746271a40214de03d0ff4cb0ee93f4c6de
Alan <boyce.alan15@gmail.com>
2019-10-22 02:50:33 -0400

    dashboard ui additions - card, card collapse, filter bar, add server button

--- ui/src/components/dashboard/Dashboard.jsx
+++ ui/src/components/dashboard/Dashboard.jsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Button, Container, Form, FormGroup, Input} from 'reactstrap';
-import socketIOClient from 'socket.io-client'
+import socketIOClient from 'socket.io-client';
+import { InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import ServerCard from '../layout/ServerCard';
 
 class Dashboard extends React.Component{
   constructor(props) {
@@ -32,8 +34,19 @@ class Dashboard extends React.Component{
       <div className="Dashboard-Component">
         <Container>
           <div>
-            
+            <InputGroup>
+              <Input placeholder="Filter" />
+              <Button color="primary" size="lg" active>+</Button>{' '}
+            </InputGroup>
           </div>
+
+          <ServerCard/>
+          <ServerCard/>
+          <ServerCard/>
+          
+
+
+          
           {/* <a href="#" onclick="signOut();">Sign out</a> */}
         </Container>
       </div>
